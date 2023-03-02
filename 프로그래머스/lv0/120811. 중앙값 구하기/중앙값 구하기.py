cd70f3cb19a618d0e4ccdda82d0fa900752b4f30
JaeHong-Park987 <jar987@naver.com>
2023-03-03 06:08:48 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,9 +1,11 @@
 def solution(array):
     sort_arr = sorted(array)
     
+    # array길이가 홀수일 때
     if len(array) % 2 != 0:
         answer = sort_arr[len(array) // 2]
-        
+    
+    # array길이가 짝수일 때
     elif len(array) % 2 == 0:
         answer = (sort_arr[(len(array) // 2)] + sort_arr[(len(array) // 2) + 1]) // 2
         
