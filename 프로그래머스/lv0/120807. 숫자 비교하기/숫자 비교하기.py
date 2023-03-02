d0f6faaf204888f0c2a80ede2e6002e49afc988c
JaeHong-Park987 <jar987@naver.com>
2023-03-02 18:45:38 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.py
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -1,7 +1,7 @@
 def solution(num1, num2):
-    if num1 == num2:
-        answer = 1
-    else:
-        answer = -1
+    if num1 == num2: # 같으면
+        answer = 1 # 1 출력
+    elif num1 != num2: # 다르면
+        answer = -1 # -1 출력
         
     return answer
