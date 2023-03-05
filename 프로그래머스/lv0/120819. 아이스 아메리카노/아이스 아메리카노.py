2e77b843610311542e192d0dea68f57ea1d998c0
JaeHong-Park987 <jar987@naver.com>
2023-03-05 17:17:54 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,8 +1,13 @@
 def solution(money):
+    # 변수 선언
     answer = []
     ice_americano = 5500
-    buy = money // ice_americano
-    charge = money % ice_americano
+    
+    buy = money // ice_americano # 마실 수 있는 아메리카노의 잔 수
+    charge = money % ice_americano # 남는 돈
+    
+    # 배열에 넣기
     answer.append(buy)
     answer.append(charge)
+    
     return answer
