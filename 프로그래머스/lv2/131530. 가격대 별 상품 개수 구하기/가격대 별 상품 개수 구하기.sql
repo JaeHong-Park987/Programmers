4ddd626aee5fc4615176c6eddccbb3ee1ba9ffe3
JaeHong-Park987 <jar987@naver.com>
2023-03-07 08:47:50 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,15 +1,18 @@
 -- 코드를 입력하세요
+# SELECT
+#     CASE WHEN PRICE < 10000 then 0
+#          WHEN PRICE < 20000 then 10000
+#          WHEN PRICE < 30000 then 20000
+#          WHEN PRICE < 40000 then 30000
+#          WHEN PRICE < 50000 then 40000
+#          WHEN PRICE < 60000 then 50000
+#          WHEN PRICE < 70000 then 60000
+#          WHEN PRICE < 80000 then 70000
+#          WHEN PRICE < 90000 then 80000
+#          END AS PRICE_GROUP,
+#     COUNT(PRODUCT_ID) AS PRODUCTS
 SELECT
-    CASE WHEN PRICE < 10000 then 0
-         WHEN PRICE < 20000 then 10000
-         WHEN PRICE < 30000 then 20000
-         WHEN PRICE < 40000 then 30000
-         WHEN PRICE < 50000 then 40000
-         WHEN PRICE < 60000 then 50000
-         WHEN PRICE < 70000 then 60000
-         WHEN PRICE < 80000 then 70000
-         WHEN PRICE < 90000 then 80000
-         END AS PRICE_GROUP,
+    TRUNCATE(PRICE, -4) AS PRICE_GROUP,
     COUNT(PRODUCT_ID) AS PRODUCTS
 FROM PRODUCT
 GROUP BY PRICE_GROUP
