14dc7dc20ad3f04ac7161446ee55c40a632eda99
JaeHong-Park987 <jar987@naver.com>
2023-03-25 11:30:37 +0900

    [level 0] Title: 배열 자르기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.py
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.py
@@ -1,3 +1,9 @@
 def solution(numbers, num1, num2):
     return numbers[num1:num2+1]
+    
+    # 이게 안되는 이유는?
+    # answer = []
+    # for i in range(num2):
+    #     answer.append(numbers[num1+i])
+    # return answer
 
