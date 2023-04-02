3faeaeb7ac26677182a9d9ff0ff7e16f618e3e84
JaeHong-Park987 <jar987@naver.com>
2023-04-03 00:39:30 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -0,0 +1,9 @@
+def solution(num_list, n):
+    answer = [] # 2차원 리스트 선언
+    k = n # n씩 나눌 수 선언
+    for i in range(int(len(num_list) / n)):
+        answer.append([])
+        for j in range(n-k, n):
+            answer[i].append(num_list[j])
+        n += k
+    return answer
