3dd2f1c88ca281721d2442b35cbac2abd030d262
JaeHong-Park987 <jar987@naver.com>
2023-04-13 18:17:29 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = []
+    d = 2
+    
+    while d <= n:
+        if n % d == 0:
+            n = int(n / d)
+            
+            if d not in answer:
+                answer.append(d)
+                
+        else:
+            d += 1
+            
+    return answer
