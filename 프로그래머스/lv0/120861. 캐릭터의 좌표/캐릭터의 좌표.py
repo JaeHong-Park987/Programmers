ae9637bd6cb025087c227807968b7173bb459d3f
JaeHong-Park987 <jar987@naver.com>
2023-04-17 14:24:29 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -3,25 +3,25 @@ def solution(keyinput, board):
     board[0] = board[0] // 2
     board[1] = board[1] // 2
     
-    for k in keyinput:
-        if k == 'left':
+    for j in keyinput:
+        if j == 'left' and answer[0] > -1 * board[0] :
             answer[0] -= 1
-        elif k == 'right':
+        elif j == 'right' and answer[0] < board[0] :
             answer[0] += 1
-        elif k == 'up':
+        elif j == 'up' and answer[1] < board[1] :
             answer[1] += 1
-        elif k == 'down':
+        elif j == 'down' and answer[1] > -1 * board[1] :
             answer[1] -= 1
             
-        while (abs(answer[0]) > board[0]):
-            if answer[0] > 0:
-                answer[0] -= 1
-            elif answer[0] < 0:
-                answer[0] += 1
+#         while (abs(answer[0]) > board[0]):
+#             if answer[0] > 0:
+#                 answer[0] -= 1
+#             elif answer[0] < 0:
+#                 answer[0] += 1
             
-        while (abs(answer[1]) > board[1]):
-            if answer[1] > 0:
-                answer[1] -= 1
-            elif answer[1] < 0:
-                answer[1] += 1
+#         while (abs(answer[1]) > board[1]):
+#             if answer[1] > 0:
+#                 answer[1] -= 1
+#             elif answer[1] < 0:
+#                 answer[1] += 1
     return answer
