5e47f5022cd9a98580cdd64a0d8b88e1bf5c7ccc
JaeHong-Park987 <jar987@naver.com>
2023-04-18 15:47:58 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,8 +1,13 @@
-def solution(numbers):
-    multi = []
-    for f in range(0, len(numbers)):
-        for s in range(f+1, len(numbers)):
-            multi.append(numbers[f] * numbers[s])
-    answer = max(multi)
+# def solution(numbers):
+#     multi = []
+#     for f in range(0, len(numbers)):
+#         for s in range(f+1, len(numbers)):
+#             multi.append(numbers[f] * numbers[s])
+#     answer = max(multi)
     
-    return answer
+#     return answer
+
+def solution(numbers):
+    numbers.sort()
+    
+    return max(numbers[-1] * numbers[-2], numbers[0] * numbers[1])
