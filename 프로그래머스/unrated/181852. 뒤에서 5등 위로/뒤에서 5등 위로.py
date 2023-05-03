186c44f9e558733716a2c644b136f950235a3607
JaeHong-Park987 <jar987@naver.com>
2023-05-03 14:57:25 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.py
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.py
@@ -2,3 +2,9 @@ def solution(num_list):
     num_list.sort()
     
     return num_list[5:]
+
+    # sorted 함수를 활용하여 푸는 방법
+    # return sorted(num_list)[5:]
+# 리스트를 오름차순으로 정렬하는 방법
+    # 1. sort()
+    # 2. sorted()
