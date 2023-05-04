d494a4c2066670df3fa6f3f44d64c782af432c28
JaeHong-Park987 <jar987@naver.com>
2023-05-04 16:05:33 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -2,9 +2,12 @@ def solution(a, b):
     ab = int(str(a) + str(b))
     ba = int(str(b) + str(a))
     
-    if ab > ba:
-        return ab
-    else:
-        return ba
+    # if ab > ba:
+    #     return ab
+    # else:
+    #     return ba
     
+    # max 함수 활용!!!
+    
+    return max(ab, ba)
     
