e502456d940e595d60d3f521c1ca983fbbf8f7e7
JaeHong-Park987 <jar987@naver.com>
2023-05-04 16:01:02 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,8 +1,10 @@
 def solution(num_list):
-    minus_list = [] # 음수 리스트
+    # minus_list = [] # 음수 리스트
     for i in range(len(num_list)):
         if num_list[i] < 0:
-            minus_list.append(num_list[i])
+            # minus_list.append(num_list[i])
             return i
-    if len(minus_list) == 0: # 음수 리스트에 아무것도 없다면 -> num_list가 모두 양수
-        return -1
+    # if len(minus_list) == 0: # 음수 리스트에 아무것도 없다면 -> num_list가 모두 양수
+    return -1
+
+# 굳이 새로운 리스트 만들 필요 없음 -> for문이 끝날때까지 return 되는 값이 없다면 for문 밖에서 -1 return 해주면 된다.
