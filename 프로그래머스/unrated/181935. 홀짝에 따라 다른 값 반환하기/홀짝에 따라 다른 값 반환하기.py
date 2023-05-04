f0546ab107bf90656da6c661b6b5fec1a0343925
JaeHong-Park987 <jar987@naver.com>
2023-05-04 15:50:24 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    if n % 2 != 0: # 홀수라면
+        for i in range(1,n+1):
+            if i % 2 != 0:
+                answer += i
+    else:
+        for i in range(1, n+1):
+            if i % 2 == 0:
+                answer += i**2
+                
+    return answer
