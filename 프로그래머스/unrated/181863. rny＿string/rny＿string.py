a82e60d4cb1f694d94b041c08a0aa089bfe3e8bb
JaeHong-Park987 <jar987@naver.com>
2023-05-05 23:09:21 +0900

    [unrated] Title: rny_string, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.py
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.py
@@ -1,10 +1,13 @@
 def solution(rny_string):
-    answer = ''
-    for i in rny_string:
-        if i == 'm':
-            i = 'rn'
-            answer += i
-        else:
-            answer += i
+#     answer = ''
+#     for i in rny_string:
+#         if i == 'm':
+#             i = 'rn'
+#             answer += i
+#         else:
+#             answer += i
             
-    return answer
+#     return answer
+
+# 간단하게 replace 함수를 사용하면 바꾸고 싶은 문자를 원하는 문자로 변경 가능하다.
+    return rny_string.replace('m', 'rn')
