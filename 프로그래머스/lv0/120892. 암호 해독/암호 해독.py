31b7dfb7ee0e5f01aab43524c374e2674be37521
JaeHong-Park987 <jar987@naver.com>
2023-05-08 14:14:32 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,9 +1,5 @@
 def solution(cipher, code):
-    cipher = list(cipher) # 암호화된 문자열을 리스트로 변환
-    real_code = '' # 실제 코드 변수 선언
-    
-    for i in range(1, len(cipher) + 1):
-        if i % code == 0:
-            real_code += cipher[i-1]
-            
-    return real_code
+    return cipher[code-1::code]
+
+# 리스트 변환할 필요 없음
+# [a::b] - a부터 끝까지 b만큼 더한 값 출력 
