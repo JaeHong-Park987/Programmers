eb1956f9e28b5bcd2658dead786f7750b729a122
JaeHong-Park987 <jar987@naver.com>
2023-05-16 16:39:53 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -1,11 +1,14 @@
 def solution(arr, delete_list):
-    answer = []
-    for i in arr:
-        if i not in delete_list:
-            answer.append(i)
-    return answer
+    for i in delete_list:
+        if i in arr:
+            arr.remove(i)
+            
+    return arr
+
 #     for i in arr:
 #         if i in delete_list:
 #             arr.remove(i)
             
 #     return arr
+# 위 방법대로 하면 arr이 지워진 시점에서 인덱스가 하나씩 당겨지기 때문에 거치지 않는 값들이 발생한다.
+# input: [1, 2, 3, 4, 5, 6, 7, 8, 9] / delete_list : [1, 2, 3] / output: [4, 5, 6, 7, 8, 9] 이걸로 검토해보기 -> 실제로 [2,4,5,6,7,8,9]로 출력도니다.
