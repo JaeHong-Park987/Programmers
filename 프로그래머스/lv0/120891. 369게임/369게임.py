bba492a80d6adede26e802078e5dc531d8651456
JaeHong-Park987 <jar987@naver.com>
2023-05-16 09:21:14 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,7 +1,7 @@
 def solution(order):
     answer = 0
     for i in str(order):
-        if i == "3" or i == "6" or i == "9":
+        if int(i) != 0 and int(i) % 3 == 0: # i가 0이면 나머지가 0이기 때문에 0일 때도 카운트가 된다. 그러므로 i가 0이 아니면서 3으로 나눴을 때 나머지가 0일 때 카운트가 되야 한다.
             answer += 1
     
     return answer
