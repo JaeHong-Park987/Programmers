32ac71b24ed7b9c01e9af9821585fb45584972be
JaeHong-Park987 <jar987@naver.com>
2023-05-16 16:11:35 +0900

    [unrated] Title: ad 제거하기, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.py
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.py
@@ -1,10 +1,8 @@
 def solution(strArr):
     answer = []
-    for i in strArr:
-        if i.find('ad') == -1:
-            answer.append(i)
     
+    for i in strArr:
+        if 'ad' not in i:
+            answer.append(i)
+            
     return answer
-
-# 문자열.find('찾고싶은문자열') -> 찾고 싶은 문자열이 전체 문자열에서 시작되는 인덱스를 출력
-# 문자열에 찾고 싶은 내용이 없다면 -1 출력
