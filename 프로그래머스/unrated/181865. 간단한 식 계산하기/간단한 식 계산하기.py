2ca9465fd53d55240666fa7d11eb06c5d1e02253
JaeHong-Park987 <jar987@naver.com>
2023-05-16 16:45:47 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,13 +1,5 @@
 def solution(binomial):
-    b_list = binomial.split(' ')
-    
-    a = int(b_list[0])
-    op = b_list[1]
-    b = int(b_list[2])
-    
-    if op == '+':
-        return a + b
-    elif op == '-':
-        return a - b
-    else:
-        return a * b
+    return eval(binomial)
+
+
+# eval 함수는 한줄로 정리하자면 매개변수로 받은 expression(우리가 아는 일반적인 사칙연산 '식')을 문자열로 받아서, 실행하는 함수다. 즉, 매개변수로 받은 expression은 파이썬에서 실행 가능한 문자열이 들어와야 한다는것이고, 문자열로 들어온 그 expression을 파이썬이 실행해주는 함수이다.
