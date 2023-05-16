6fd60b90a52c2023ac762479f8f1d68a9506622f
JaeHong-Park987 <jar987@naver.com>
2023-05-16 16:01:41 +0900

    [unrated] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.py
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.py
@@ -1,11 +1,11 @@
 def solution(arr1, arr2):
-    if len(arr1) > len(arr2):
+    if len(arr1) > len(arr2): # arr1 길이가 길면
         return 1
-    elif len(arr1) < len(arr2):
+    elif len(arr1) < len(arr2): # arr2 길이가 길면
         return -1
-    else:
-        if sum(arr1) > sum(arr2):
+    else: # 길이가 같다면
+        if sum(arr1) > sum(arr2): # arr1 합이 크면
             return 1
-        elif sum(arr1) < sum(arr2):
+        elif sum(arr1) < sum(arr2): # arr2 합이 크면
             return -1
-    return 0
+    return 0 # 
