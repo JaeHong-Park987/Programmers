bf94e71c41801a471e14728dceb7a79deca1e2cb
JaeHong-Park987 <jar987@naver.com>
2023-05-20 14:17:54 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,4 +1,2 @@
 def solution(seoul):
-    for i, name in enumerate(seoul):
-        if name == 'Kim':
-            return '김서방은 {}에 있다'.format(i)
+    return '김서방은 {}에 있다'.format(seoul.index('Kim'))
