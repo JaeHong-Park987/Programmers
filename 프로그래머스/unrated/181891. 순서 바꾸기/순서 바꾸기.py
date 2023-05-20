344a81a5392d401df1124d6725a7e0d37e01f747
JaeHong-Park987 <jar987@naver.com>
2023-05-20 14:07:01 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.py
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.py
@@ -1,9 +1,2 @@
 def solution(num_list, n):
-    answer = []
-    for i in range(n, len(num_list)):
-        answer.append(num_list[i])
-    
-    for j in range(n):
-        answer.append(num_list[j])
-        
-    return answer
+    return num_list[n:] + num_list[:n]
