d35cd604e4c3541cea3194ac416604789c0f2b0c
JaeHong-Park987 <jar987@naver.com>
2023-05-22 05:53:08 +0900

    [unrated] Title: 수 조작하기 2, Time: 13.60 ms, Memory: 12.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,16 +1,15 @@
 def solution(numLog):
     answer = ''
     for i in range(1, len(numLog)):
-        b_num = numLog[i-1]
-        a_num = numLog[i]
+        dif = numLog[i] - numLog[i-1]
         
-        if (a_num - b_num) == 1:
+        if dif == 1:
             answer += 'w'
-        elif (a_num - b_num) == -1:
+        elif dif == -1:
             answer += 's'
-        elif (a_num - b_num) == 10:
+        elif dif == 10:
             answer += 'd'
-        elif (a_num - b_num) == -10:
+        elif dif == -10:
             answer += 'a'
     
     return answer
