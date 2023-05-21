3929fada5743f154e8334e6a3f03f5ed7bda7911
JaeHong-Park987 <jar987@naver.com>
2023-05-21 21:26:14 +0900

    [unrated] Title: 문자열 잘라서 정렬하기, Time: 4.54 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -0,0 +1,9 @@
+def solution(myString):
+    answer = []
+    for i in sorted(myString.split('x')):
+        if i != '':
+            answer.append(i)
+            
+    return answer
+            
+        
