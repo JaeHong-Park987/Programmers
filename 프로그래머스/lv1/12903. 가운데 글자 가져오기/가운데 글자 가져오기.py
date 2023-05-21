316c980d6ac9a1b717db1da72b0e42fa47396765
JaeHong-Park987 <jar987@naver.com>
2023-05-22 06:01:08 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,8 +1,8 @@
 def solution(s):
-    middle_num = int(len(s) / 2)
+    middle_num = int(len(s) / 2) # len(s) // 2로도 가능하다.
     
     if (len(s) % 2) != 0:
-        return s[int(middle_num)]
+        return s[middle_num]
     else:
         return s[middle_num-1:middle_num+1]
         
