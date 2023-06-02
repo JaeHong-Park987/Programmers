ed52a670073f6c4de1767596544850b8c7e7b864
JaeHong-Park987 <jar987@naver.com>
2023-06-02 18:51:56 +0900

    [unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -1,7 +1,2 @@
 def solution(my_string, m, c):
-    answer = ''
-    
-    for i in range(0, len(my_string), m):
-        answer += my_string[0+i:m+i][c-1]
-    
-    return answer
+    return my_string[c-1::m]
