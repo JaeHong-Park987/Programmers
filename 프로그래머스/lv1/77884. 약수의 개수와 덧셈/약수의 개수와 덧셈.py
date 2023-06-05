fa95ac9638d744a1bdae51aaf907ee7c84f9eb42
JaeHong-Park987 <jar987@naver.com>
2023-06-05 11:19:00 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -2,13 +2,10 @@ def solution(left, right):
     answer = 0
     
     for i in range(left,right+1):
-        count = 0
-        for j in range(1,i+1):
-            if i % j == 0:
-                count += 1
-        if count % 2 == 0:
-            answer += i
-        else:
+        if int(i**0.5) == i ** 0.5:
             answer -= i
+        else:
+            answer += i
     
     return answer
+# 제곱수라면 약수의 수가 짝수이다.
