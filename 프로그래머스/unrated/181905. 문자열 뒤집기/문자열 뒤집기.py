883b7daa411863d2703be34f7e196b7801f22d3f
JaeHong-Park987 <jar987@naver.com>
2023-06-05 09:22:24 +0900

    [unrated] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,10 +1,2 @@
 def solution(my_string, s, e):
-    answer = ''
-    
-    for i in range(len(my_string)):
-        if s<= i <= e:
-            answer += my_string[s+e-i]
-        else:
-            answer += my_string[i]
-
-    return answer
+    return my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
