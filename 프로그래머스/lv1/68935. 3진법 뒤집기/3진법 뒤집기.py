66c62ad179bdfab546b5a80a7763b9d8e6891efc
JaeHong-Park987 <jar987@naver.com>
2023-06-25 09:47:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    r_num_3 = '' # 3진법(앞뒤 반전)
+    answer = 0
+    
+    while n != 0:
+        r = n % 3 # 나머지
+        n = n // 3 # 몫
+        r_num_3 += str(r)
+        
+    for i in range(len(r_num_3)):
+        answer += (3 ** (len(r_num_3) - 1 - i)) * int(r_num_3[i])
+    
+    return answer
+# 1 3 9 27 81
