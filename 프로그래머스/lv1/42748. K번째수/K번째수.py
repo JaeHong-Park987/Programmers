32a03cb2f2587c96c4b33d05ef4c7a25e7c3bd65
JaeHong-Park987 <jar987@naver.com>
2023-07-08 18:36:26 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,6 +1,2 @@
 def solution(array, commands):
-    answer = []
-    for arr in commands:
-        answer.append(sorted(array[arr[0]-1:arr[1]])[arr[2]-1])
-    
-    return answer
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
