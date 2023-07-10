aaeabee8c2a1f2fd8617ada8f598d9589a0d1e8c
JaeHong-Park987 <jar987@naver.com>
2023-07-11 08:35:14 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.35 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,15 @@
+def solution(numbers):
+    answer = []
+    numbers.sort()
+    
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            total = numbers[i]+numbers[j]
+            
+            if total not in answer:
+                answer.append(total)
+                
+    answer.sort()
+    
+    return answer
+    
