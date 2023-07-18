f001c0cf27884d137a35578b6b51ef3b45818e2a
JaeHong-Park987 <jar987@naver.com>
2023-07-18 12:47:52 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,12 +1,9 @@
-import math
-
 def solution(n):
-    count = 0
-    for i in range(n+1):
-        j = n - i
-        if j >= i:
-            count += math.comb(j,i)
+    a = 0
+    b = 1
+    for i in range(n):
+        a, b = b, a+b
     
-    return count % 1234567
+    return b % 1234567
         
-        
+# math에 내장된 조합 함수를 사용해서 푸는 것 보다 훨씬 효율적이다. 문제가 피보나치 형태로 간다는 것을 발견하는게 어려울뿐...
