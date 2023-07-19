832afca2a787974e3c77c24d893c94277b68bfce
JaeHong-Park987 <jar987@naver.com>
2023-07-19 09:30:58 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,26 +1,22 @@
 -- 코드를 입력하세요
+# 13 39
 SELECT
-    P.MEMBER_NAME,
-    R.REVIEW_TEXT,
-    DATE_FORMAT(R.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
-FROM MEMBER_PROFILE AS P
-JOIN REST_REVIEW AS R
-ON P.MEMBER_ID = R.MEMBER_ID
-WHERE P.MEMBER_ID IN (SELECT
-                        MEMBER_ID
-                     FROM REST_REVIEW
-                     GROUP BY MEMBER_ID
-                     HAVING COUNT(*) = (SELECT MAX(COUNTED)
-                                        FROM (SELECT COUNT(*) AS COUNTED
-                                              FROM REST_REVIEW
-                                              GROUP BY MEMBER_ID) AS COUNT))
-ORDER BY R.REVIEW_DATE, R.REVIEW_TEXT                                              
-
-# SELECT
-#     MEMBER_ID
-# FROM REST_REVIEW
-# GROUP BY MEMBER_ID
-# HAVING COUNT(*) = (SELECT MAX(COUNTED)
-#                    FROM (SELECT COUNT(*) AS COUNTED
-#                          FROM REST_REVIEW
-#                          GROUP BY MEMBER_ID) AS COUNT)
+    MEMBER_NAME,
+    REVIEW_TEXT,
+    DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+FROM REST_REVIEW AS R1
+JOIN
+(SELECT
+    MEMBER_ID
+FROM REST_REVIEW
+GROUP BY MEMBER_ID
+HAVING COUNT(MEMBER_ID) = (SELECT COUNT(MEMBER_ID)
+                           FROM REST_REVIEW
+                           GROUP BY MEMBER_ID
+                           ORDER BY COUNT(MEMBER_ID) DESC
+                           LIMIT 1)
+) AS R2
+ON R1.MEMBER_ID = R2.MEMBER_ID
+JOIN MEMBER_PROFILE AS M
+ON R1.MEMBER_ID = M.MEMBER_ID
+ORDER BY REVIEW_DATE
