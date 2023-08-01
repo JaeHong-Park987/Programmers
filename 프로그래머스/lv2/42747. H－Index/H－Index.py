392fc06a963880e69998fb808597a3d1568b7d9a
JaeHong-Park987 <jar987@naver.com>
2023-08-02 08:34:29 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,15 +1,7 @@
 def solution(citations):
-    count_list = []
-    
-    for i in range(1, len(citations)+1):
-        count = 0
-        for j in citations:
-            if i <= j:
-                count += 1
-        if i <= count:
-            count_list.append(i)
-    
-    if len(count_list) == 0:
-        return 0
-    else:
-        return max(count_list)
+    citations = sorted(citations)
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
