ac6868feb9b2d40811a1855c84701f15a9ee3e3b
JaeHong-Park987 <jar987@naver.com>
2023-08-01 10:52:07 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -6,31 +6,6 @@ def solution(a, b):
            5 : 'TUE',
            6 : 'WED',
            0 : 'THU'}
+    day = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
-    if a == 1: # 31
-        return dic[b % 7]
-    elif a == 2: # 29
-        return dic[(31+b) % 7]
-    elif a == 3: # 31
-        return dic[(60+b) % 7]
-    elif a == 4: # 30
-        return dic[(91+b) % 7]
-    elif a == 5: # 31
-        return dic[(121+b) % 7]
-    elif a == 6: # 30 
-        return dic[(152+b) % 7]
-    elif a == 7: # 31
-        return dic[(182+b) % 7]
-    elif a == 8: # 31
-        return dic[(213+b) % 7]
-    elif a == 9: # 30
-        return dic[(244+b) % 7]
-    elif a == 10: # 31
-        return dic[(274+b) % 7]
-    elif a == 11: # 30
-        return dic[(305+b) % 7]
-    elif a == 12: # 31
-        return dic[(335+b) % 7]
-
-# 31 29 31 30 31 30 31 31 30 31 30 31
-# 366
+    return dic[(sum(day[:a-1]) + b) % 7]
